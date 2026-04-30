5a507028896125ddfab81bd1a8cfa8dad91532b0
k <k@k.k>
2026-04-30 11:47:56 +0300

    выполнены обе части

--- index.php
+++ index.php
@@ -1,105 +1,36 @@
 <?php
 // 1
-$filename1 = 'test.txt';
-$file1 = fopen($filename1, 'w');
-if ($file1) {
-    fwrite($file1, 'Привет, мир');
-    fclose($file1);
-    echo "Файл '$filename1' создан и записан.<br>";
-} else {
-    echo "Ошибка создания файла '$filename1'.<br>";
+try {
+    $file = fopen("nonexistent_file.txt", "r");
+    if (!$file) {
+        throw new Exception("Не удалось открыть файл 'nonexistent_file.txt'");
+    }
+    fclose($file);
+} catch (Exception $ex) {
+    echo "1. Исключение: " . $ex->getMessage() . "<br>";
 }
 
 // 2
-if (file_exists($filename1)) {
-    $file1_read = fopen($filename1, 'r');
-    if ($file1_read) {
-        $content = fgets($file1_read);
-        fclose($file1_read);
-        echo "Содержимое '$filename1': " . $content . "<br>";
+try {
+    $divisor = 0;
+    if ($divisor == 0) {
+        throw new Exception("Деление на ноль");
     }
-} else {
-    echo "Файл '$filename1' не найден для чтения.<br>";
+    $result = 10 / $divisor;
+} catch (Exception $ex) {
+    $logMessage = date('Y-m-d H:i:s') . " - Исключение: " . $ex->getMessage() . "\n";
+    file_put_contents("log.txt", $logMessage, FILE_APPEND);
+    echo "2. Исключение записано в log.txt: " . $ex->getMessage() . "<br>";
 }
 
 // 3
-$oldname = 'test.txt';
-$newname = 'mir.txt';
-if (file_exists($oldname)) {
-    if (rename($oldname, $newname)) {
-        echo "Файл '$oldname' переименован в '$newname'.<br>";
-    } else {
-        echo "Ошибка переименования файла.<br>";
+try {
+    $countries = ['Spain' => 'Madrid', 'Russia' => 'Moscow'];
+    $key = 'Germany';
+    if (!array_key_exists($key, $countries)) {
+        throw new Exception("Ключ '$key' не существует в массиве");
     }
-} else {
-    echo "Файл '$oldname' не существует для переименования.<br>";
+    echo $countries[$key];
+} catch (Exception $ex) {
+    echo "3. Исключение: " . $ex->getMessage() . "<br>";
 }
-
-// 4
-$dir = 'folder';
-if (!file_exists($dir)) {
-    if (mkdir($dir)) {
-        echo "Папка '$dir' создана.<br>";
-    } else {
-        echo "Ошибка создания папки '$dir'.<br>";
-    }
-}
-
-$file_to_move = 'mir.txt';
-$destination = $dir . '/' . $file_to_move;
-if (file_exists($file_to_move)) {
-    if (rename($file_to_move, $destination)) {
-        echo "Файл '$file_to_move' перемещен в '$destination'.<br>";
-    } else {
-        echo "Ошибка перемещения файла.<br>";
-    }
-} else {
-    echo "Файл '$file_to_move' не найден для перемещения.<br>";
-}
-
-// 5
-$source = $dir . '/mir.txt';
-$copy_name = $dir . '/world.txt';
-if (file_exists($source)) {
-    if (copy($source, $copy_name)) {
-        echo "Файл '$source' скопирован в '$copy_name'.<br>";
-    } else {
-        echo "Ошибка копирования файла.<br>";
-    }
-} else {
-    echo "Исходный файл '$source' не найден для копирования.<br>";
-}
-
-// 6
-$world_file = $dir . '/world.txt';
-if (file_exists($world_file)) {
-    $size_bytes = filesize($world_file);
-    $size_mb = $size_bytes / (1024 * 1024);
-    $size_gb = $size_bytes / (1024 * 1024 * 1024);
-    
-    echo "Размер файла 'world.txt':<br>";
-    echo "В байтах: $size_bytes<br>";
-    echo "В мегабайтах: " . number_format($size_mb, 6) . " MB<br>";
-    echo "В гигабайтах: " . number_format($size_gb, 9) . " GB<br>";
-} else {
-    echo "Файл '$world_file' не найден для определения размера.<br>";
-}
-
-// 7
-if (file_exists($world_file)) {
-    if (unlink($world_file)) {
-        echo "Файл '$world_file' успешно удален.<br>";
-    } else {
-        echo "Ошибка удаления файла '$world_file'.<br>";
-    }
-} else {
-    echo "Файл '$world_file' не существует для удаления.<br>";
-}
-
-// 8
-$check_world = $dir . '/world.txt';
-$check_mir = $dir . '/mir.txt';
-
-echo "Проверка существования файлов:<br>";
-echo "'$check_world' существует? - " . (file_exists($check_world) ? 'Да' : 'Нет') . "<br>";
-echo "'$check_mir' существует? - " . (file_exists($check_mir) ? 'Да' : 'Нет') . "<br>";
--- index2.php
+++ index2.php
@@ -1,82 +1,50 @@
 <?php
 // 1
-$dir_test = 'test';
-if (!file_exists($dir_test)) {
-    if (mkdir($dir_test)) {
-        echo "Папка '$dir_test' создана.<br>";
-    } else {
-        echo "Ошибка создания папки '$dir_test'.<br>";
-    }
-} else {
-    echo "Папка '$dir_test' уже существует.<br>";
-}
+echo "1. Timestamp: " . mktime(10, 25, 0, 3, 15, 2025) . "<br>";
 
 // 2
-$dir_old = 'test';
-$dir_new = 'www';
-if (file_exists($dir_old) && !file_exists($dir_new)) {
-    if (rename($dir_old, $dir_new)) {
-        echo "Папка '$dir_old' переименована в '$dir_new'.<br>";
-    } else {
-        echo "Ошибка переименования папки.<br>";
-    }
-} elseif (file_exists($dir_new)) {
-    echo "Папка '$dir_new' уже существует. Переименование невозможно.<br>";
-} else {
-    echo "Папка '$dir_old' не найдена для переименования.<br>";
-}
+$past = mktime(8, 5, 59, 10, 2, 1990);
+echo "2. Разница в секундах: " . (time() - $past) . "<br>";
 
 // 3
-$dir_to_delete = 'www';
-if (file_exists($dir_to_delete)) {
-    $files_in_dir = scandir($dir_to_delete);
-    foreach ($files_in_dir as $item) {
-        if ($item != '.' && $item != '..') {
-            $path = $dir_to_delete . '/' . $item;
-            if (is_file($path)) {
-                unlink($path);
-            } elseif (is_dir($path)) {
-                rmdir($path);
-            }
-        }
-    }
-    if (rmdir($dir_to_delete)) {
-        echo "Папка '$dir_to_delete' успешно удалена.<br>";
-    } else {
-        echo "Ошибка удаления папки '$dir_to_delete'.<br>";
-    }
-} else {
-    echo "Папка '$dir_to_delete' не существует.<br>";
-}
+echo "3. Текущая дата-время: " . date('Y.m.d H:i:s') . "<br>";
 
 // 4
-$folders_to_create = ['images', 'css', 'js', 'uploads'];
-$parent_dir = 'test';
-if (!file_exists($parent_dir)) {
-    mkdir($parent_dir);
-    echo "Создана родительская папка '$parent_dir'.<br>";
-}
-
-foreach ($folders_to_create as $folder) {
-    $path = $parent_dir . '/' . $folder;
-    if (!file_exists($path)) {
-        if (mkdir($path)) {
-            echo "Создана вложенная папка: '$path'.<br>";
-        } else {
-            echo "Ошибка создания папки '$path'.<br>";
-        }
-    } else {
-        echo "Папка '$path' уже существует.<br>";
-    }
-}
+echo "4. 1 сентября: " . date('Y.m.d', mktime(0, 0, 0, 9, 1, date('Y'))) . "<br>";
 
 // 5
-echo "<br><strong>Файлы с расширением .jpg в текущей папке:</strong><br>";
-$jpg_files = glob("*.jpg");
-if (count($jpg_files) > 0) {
-    foreach ($jpg_files as $jpg) {
-        echo "Найден файл: $jpg<br>";
-    }
-} else {
-    echo "Файлы .jpg не найдены в текущей папке.<br>";
-}
+$weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+$dayOfWeek = date('w', mktime(0, 0, 0, 2, 2, 2000));
+echo "5. 2 февраля 2000 года: " . $weekDays[$dayOfWeek] . "<br>";
+
+// 6
+$week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+echo "6. Текущий день недели: " . $week[date('w')] . "<br>";
+echo "6. 12.06.2016: " . $week[date('w', mktime(0, 0, 0, 6, 12, 2016))] . "<br>";
+echo "6. 30.01.2007: " . $week[date('w', mktime(0, 0, 0, 1, 30, 2007))] . "<br>";
+
+// 7
+$date1 = '2025-12-31';
+$date2 = '2025-03-08';
+$timestamp1 = strtotime($date1);
+$timestamp2 = strtotime($date2);
+$greater = ($timestamp1 > $timestamp2) ? $date1 : $date2;
+echo "7. Большая дата из '$date1' и '$date2': $greater<br>";
+
+// 8
+$inputDate = '2025-06-15';
+echo "8. Преобразованная дата '$inputDate': " . date('d-m-Y', strtotime($inputDate)) . "<br>";
+
+// 9
+$date = date_create('2000-02-03');
+date_modify($date, '+2 days +1 month +3 days +1 year');
+echo "9. Прибавление: " . date_format($date, 'd.m.Y') . "<br>";
+$date = date_create('2000-02-03');
+date_modify($date, '-3 days');
+echo "9. Отнимание 3 дней: " . date_format($date, 'd.m.Y') . "<br>";
+
+// 10
+$now = time();
+$nextNewYear = mktime(0, 0, 0, 1, 1, date('Y') + 1);
+$diffSeconds = $nextNewYear - $now;
+echo "10. Дней до Нового Года: " . ceil($diffSeconds / (60 * 60 * 24)) . "<br>";
